7a02e310bff3580f6c88ea1f58af8c6849932ea1
heriwei <617443938@qq.com>
2019-10-29 17:32:43 +0800

    1.添加推流布局

--- app/src/main/java/com/hrw/minevideocall/MainActivity.java
+++ app/src/main/java/com/hrw/minevideocall/MainActivity.java
@@ -1,7 +1,9 @@
 package com.hrw.minevideocall;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.bt_accept_video_call).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, VideoCallActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
